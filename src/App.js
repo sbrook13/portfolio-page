3c61dd45b072b5dbe6fcbc357557475e9339face
sbrook13 <sbrook13@gmail.com>
2020-12-13 01:22:36 -0700

    basic header and project section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
+import ProjectSection from './components/ProjectSection'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ProjectSection />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,21 +6,45 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 function Header () {
 
+  const iconStyle = {
+    color: "white",
+    padding: "1rem",
+  }
+
+  const mainContent = {
+    height: "80vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100vw"
+  }
+
+  const rowStyle ={
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  }
+
   return (
     <header className="App-header">
-      <h1>Shelley Brook</h1>
-      <div>
-        <a href="mailto:sbrook13@gmail.com">
-          <FontAwesomeIcon icon={faEnvelope} size="2x" color="white"/>
-        </a>
-        <a href="https://github.com/sbrook13">
-          <FontAwesomeIcon icon={faGithub} size="2x" color="white"/>
-        </a>
-        <a href="https://www.linkedin.com/in/sbrook13/">
-          <FontAwesomeIcon icon={faLinkedin} size="2x" color="white"/>
-        </a>
+      <div style={mainContent}>
+        <h1>Shelley Brook</h1>
+        <p>Full Stack Software Engineer</p>
+        <div style={rowStyle}>
+          <a href="mailto:sbrook13@gmail.com">
+            <FontAwesomeIcon icon={faEnvelope} size="2x" color="white" style={iconStyle}/>
+          </a>
+          <a href="https://github.com/sbrook13">
+            <FontAwesomeIcon icon={faGithub} size="2x" color="white" style={iconStyle}/>
+          </a>
+          <a href="https://www.linkedin.com/in/sbrook13/">
+            <FontAwesomeIcon icon={faLinkedin} size="2x" color="white" style={iconStyle}/>
+          </a>
+        </div>
       </div>
-      <FontAwesomeIcon icon={faChevronDown} size="2x" color="white" />
+      <FontAwesomeIcon icon={faChevronDown} size="2x" color="white" className="bounce arrow"/>
     </header>
   );
 }
